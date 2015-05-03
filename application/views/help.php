b2b2785d09e051c42e7a1b410c7cf34b06da1fd3
Sagun Dhakhwa <sagun@outlines-rnd.com>
2015-05-03 18:11:37 +0545

    test

--- application/views/help.php
+++ application/views/help.php
@@ -10,8 +10,12 @@
                 </h1>
                 
                 You can help using these stuffs.
+                 <iframe style="width:100%;height:800px;" 
+                 src="https://docs.google.com/spreadsheets/d/1JTqImvItE9whEm3-lhd8Ir69Ug7Hq5SKHorx_wkJAFk/pubhtml?gid=0&single=true"></iframe> 
+                
             </div>
         </div>
         
+         
          
 

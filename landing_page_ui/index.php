7fa2c664ab35f00f9697e858f3074c45a6d379df
pratik <test@mail.com>
2015-05-05 17:27:54 +0545

    landing page and districts page ui

---
+++ landing_page_ui/index.php
@@ -0,0 +1,201 @@
+<?php 
+	include('header.php');
+	include('menu.php');
+?>
+    <header>
+        <div class="header-content">
+            <div class="header-content-inner">
+                <h1>Nepal Earthquake 2015 Knowledge Management System (KMS)</h1>
+                <hr>
+                <p>Total Death: <strong>5489</strong>. Total Injured:<strong>10225</strong>.<br> 
+                	Mostly Affected Districts: <strong>Sindhupalchowk,Gorkha and Bagmati</strong></p>
+                <a href="#about" class="btn btn-primary btn-xl page-scroll">Find Out More</a>
+            </div>
+        </div>
+    </header>
+
+    <section class="bg-primary" id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">We've got what you need!</h2>
+                    <hr class="light">
+                    <p class="text-faded">This is an attempt to very quickly realize a Knowledge Management System(KMS) which you can use to match demands, supplies, supports and needs in the earthquake victim relief endeavors, where everyone in the world is with Nepal.</p>
+                    <a href="#" class="btn btn-default btn-xl">Read More!</a>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="services">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 text-center">
+                    <h2 class="section-heading">Help the Earthquake victims</h2>
+                    <hr class="primary">
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-users wow bounceIn text-primary"></i>
+                        <h3>Victims</h3>
+                        <p class="text-muted">There are more than 8 millions people are affected by earthquake. Many are from very rural area of Nepal.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-thumbs-up wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                        <h3>Volunteer</h3>
+                        <p class="text-muted">Even with your small effort many people can live better. Lets volunteer and help.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-usd wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                        <h3>Donate</h3>
+                        <p class="text-muted">Your donation shouldn't be wasted, we will help you to donate in very needy area of country.</p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6 text-center">
+                    <div class="service-box">
+                        <i class="fa fa-4x fa-map-marker wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                        <h3>Affected Areas</h3>
+                        <p class="text-muted">Almost all 75 districts are affected by earthquake. View all in detail.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="no-padding" id="portfolio">
+        <div class="container-fluid">
+            <div class="row no-gutter">
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/quakes/1.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Bagmati
+                                </div>
+                                <div class="project-name">
+                                    Kathmandu
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/quakes/2.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Seti
+                                </div>
+                                <div class="project-name">
+                                    Achham
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/quakes/3.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Lumbini
+                                </div>
+                                <div class="project-name">
+                                    Argakhanchi
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/quakes/6.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Bagmati
+                                </div>
+                                <div class="project-name">
+                                    Bhaktapur
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/quakes/4.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Dhaulagiri
+                                </div>
+                                <div class="project-name">
+                                    Baglung
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-lg-4 col-sm-6">
+                    <a href="#" class="portfolio-box">
+                        <img src="img/quakes/5.jpg" class="img-responsive" alt="">
+                        <div class="portfolio-box-caption">
+                            <div class="portfolio-box-caption-content">
+                                <div class="project-category text-faded">
+                                    Mahakali
+                                </div>
+                                <div class="project-name">
+                                    Baitadi
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                
+            </div>
+        </div>
+    </section>
+
+    <aside class="bg-dark">
+        <div class="container text-center">
+            <div class="call-to-action">
+                <h2>Let's help, even small effort can make big changes. </h2>
+                <a href="#" class="btn btn-default btn-xl wow tada">Join Us Now!</a>
+            </div>
+        </div>
+    </aside>
+
+    <section id="contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 text-center">
+                    <h2 class="section-heading">Let's Get In Touch!</h2>
+                    <hr class="primary">
+                    <p>Ready to join with us? That's great! Join us or give us a call or just send us an email and we will get back to you as soon as possible!</p>
+                </div>
+                <div class="col-lg-4 col-lg-offset-2 text-center">
+                    <i class="fa fa-phone fa-3x wow bounceIn"></i>
+                    <p>123-456-6789</p>
+                </div>
+                <div class="col-lg-4 text-center">
+                    <i class="fa fa-envelope-o fa-3x wow bounceIn" data-wow-delay=".1s"></i>
+                    <p><a href="mailto:your-email@your-domain.com">feedback@nepalkms.com</a></p>
+                </div>
+            </div>
+        </div>
+    </section>
+<?php 
+	include('footer.php');
+?>

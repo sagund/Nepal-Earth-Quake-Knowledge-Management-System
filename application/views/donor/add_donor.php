9890cd933caf84c2f26fac61c4c9d46d7ee101ed
Nilesh Dosooye <nilesh.dosooye@gmail.com>
2015-05-05 22:04:49 -0500

    removed merge diff

--- application/views/donor/add_donor.php
+++ application/views/donor/add_donor.php
@@ -14,7 +14,7 @@
                                 <input type="text" id="item_name" name="item_name" placeholder="Item Name" class="form-control input-lg" required>
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="item_type">Item Type</label>
                             <div class="controls">
@@ -26,7 +26,7 @@
                                 </select>
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="item_unit">Unit for Quantity</label>
                             <div class="controls">
@@ -40,11 +40,11 @@
                                     <option>Quintel</option>
                                     <option>Ton</option>
                                     <option>Carton</option>
-                                    
+
                                 </select>
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="item_quantity">Item Quantity</label>
                             <div class="controls">
@@ -52,108 +52,14 @@
                             </div>
                         </div>
 
-<<<<<<< HEAD
-<div class="well">
-    <div id="myTabContent" class="tab-content">
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-12">
-					<form class="form-horizontal" action='' enctype="multipart/form-data" method="POST">
-						<fieldset>
-							<div id="legend">
-					            <legend class="">Donation Information</legend>
-					        </div>
-							<div class="control-group">
-			                    <label class="control-label"  for="item_name">Item Name</label>
-			                    <div class="controls">
-			                        <input type="text" name="item_name" value="" placeholder="Item Name" class="input-xlarge" required>
-			                    </div>
-			                </div>
 
-			                <div class="control-group">
-			                    <label class="control-label"  for="item_desc">Item Description</label>
-			                    <div class="controls">
-			                        <input type="text" name="item_desc" value="" placeholder="Item Description"class="input-xlarge" required>
-			                    </div>
-			                </div>
-
-			                <div class="control-group">
-			                    <label class="control-label"  for="item_type">Item Type</label>
-			                    <div class="controls">
-			                        <input type="text" name="item_type" value="" placeholder="Item Type" class="input-xlarge" required>
-			                    </div>
-			                </div>
-
-			                <div class="control-group">
-			                    <label class="control-label"  for="item_qty">Quantity</label>
-			                    <div class="controls">
-			                        <input type="text" name="item_qty" value="" placeholder="Item Quantity" class="input-xlarge" required>
-			                    </div>
-			                </div>
-
-			                <div class="control-group">
-			                    <label class="control-label"  for="collection_point">Collection Point</label>
-			                    <div class="controls">
-			                        <input type="text" name="collection_point" value="" placeholder="Where to collect" class="input-xlarge" required>
-			                    </div>
-			                </div>
-
-			                <div class="control-group">
-			                    <label class="control-label"  for="contact_number">Contact Number</label>
-			                    <div class="controls">
-			                        <input type="text" name="contact_number" value="" placeholder="Whom to Contact" class="input-xlarge" required>
-			                    </div>
-			                </div>
-
-			                <!-- <div class="control-group">
-			                    <label class="control-label"  for="media_type">Media Upload Type</label>
-			                    <div class="controls">
-			                    	<select name="media_type" class="input-xlarge">
-			                    		<option > </option>
-			                    		<option value="image"> Image </option>
-			                    		<option value="video"> Video </option>
-			                    		<option value="document"> Document </option>
-			                    		<option value="audio"> Audio File </option>
-
-			                    	</select>
-
-			                    </div>
-			                </div> -->
-			                <div class="control-group">
-			                    <label class="control-label"  for="media">Media</label>
-			                    <div class="controls">
-			                        <input type="file" name="media" class="input-xlarge">
-			                    </div>
-			                </div>
-
-			                <div class="control-group">
-			                    <label class="control-label"  for="status">Status</label>
-			                    <div class="controls">
-			                        <input type="text" name="status" value="" placeholder="Item status (i.e available, on-the-way, etc..." class="input-xlarge">
-			                    </div>
-			                </div>
-			                <div class="control-group">
-			                	<div class="controls">
-					                <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-							   		<button type="reset" class="btn btn-primary">Reset Form</button>
-							   	</div>
-						   	</div>
-						</fieldset>
-					</form>
-
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
-=======
                         <div class="control-group">
                             <label class="control-label" for="item_description">Item Description</label>
                             <div class="controls">
                                 <textarea id="item_description" name="item_description" placeholder="Enter a short description about the item you are donating here." class="form-control input-lg"></textarea>
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="collection_point">Collection Point</label>
                             <div class="controls">
@@ -167,14 +73,14 @@
                                 <input type="text" id="donor_name" name="donor_name" placeholder="Donor Full Name" class="form-control input-lg" required>
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="donor_phone">Donor Contact No.</label>
                             <div class="controls">
                                 <input type="tel" id="donor_phone" name="donor_phone" placeholder="Donor Contact No." class="form-control input-lg">
                             </div>
                         </div>
-                        
+
                         <div class="control-group">
                             <label class="control-label" for="item_status">Item Status</label>
                             <div class="controls">
@@ -187,9 +93,9 @@
                                 </select>
                             </div>
                         </div>
-                        
+
                     </div>
-                
+
                     <div class="control-group">
                         <!-- Button -->
                         <div class="controls">

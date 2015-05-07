50f727fd51eaf74cae980eae5444037842f4c152
biju <bezunakarmi@gmail.com>
2015-05-07 18:00:36 +0545

    msg

---
+++ db/update_district_records.sql
@@ -0,0 +1,84 @@
+DROP TABLE IF EXISTS district_records;
+CREATE TABLE IF NOT EXISTS district_records(
+  id INTEGER(4) NOT NULL PRIMARY KEY 
+, district_name INTEGER(4)
+, death INTEGER(11)
+, injured INTEGER(11)
+, house_destroyed INTEGER(11)
+, partial_destroyed INTEGER(11)
+);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (1,"Sindhupalchowk",2991,852,44310,18991);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (2,"Kathmandu",1210,4634,27640,33215);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (3,"Nuwakot",950,1312,30000,15000);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (4,"Dhading",702,698,20000,15000);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (5,"RASUWA",459,748,8000,1000);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (6,"Gorkha",412,1034,44607,13236);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (7,"Kavrepalanchowk",318,2780,30000,18545);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (8,"Bhaktapur",314,1887,7000,2000);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (9,"Lalitpur",174,1224,16344,5851);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (10,"Dolakha",75,272,5000,35000);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (11,"Makawanpur",33,127,363,497);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (12,"Ramechhap",27,27,17072,23149);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (13,"Solukhumbu",22,80,2483,6176);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (14,"Okhaldhunga",19,53,8000,10000);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (15,"Sindhuli",10,63,4159,8545);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (16,"Sunsari",7,25,2,21);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (17,"Chitawan",7,95,2046,3217);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (18,"Bara",5,29,7,49);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (19,"Parsa",5,23,16,20);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (20,"Lamjung",5,31,7430,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (21,"Morang",2,45,2,40);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (22,"Bhojpur",2,8,1309,2195);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (23,"Kaski",2,30,174,700);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (24,"Taplejung",1,3,3,338);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (25,"Dhanusha",1,19,1,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (26,"Sarlahi",1,50,56,313);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (27,"Rautahat",1,24,24,170);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (28,"Nawalparasi",1,13,17,68);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (29,"Tanahu",1,18,3377,12927);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (30,"Rolpa",1,5,42,80);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (31,"Rukum",1,1,61,120);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (32,"Panchthar",0,5,41,481);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (33,"Ilaam",0,5,93,393);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (34,"Jhapa",0,19,1,29);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (35,"Dhankuta",0,2,15,85);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (36,"Terhathum",0,1,162,507);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (37,"Shankhuwasabha",0,5,326,766);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (38,"Khotang",0,3,1983,4764);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (39,"Udayapur",0,28,95,205);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (40,"Siraha",0,24,0,57);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (41,"Saptari",0,16,0,47);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (42,"Mahottari",0,9,43,38);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (43,"Rupandehi",0,30,10,1043);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (44,"Kapilbastu",0,3,0,24);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (45,"Palpa",0,15,1060,1510);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (46,"Arghakhanchi",0,2,140,485);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (47,"Gulmi",0,6,770,2629);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (48,"Shyanja",0,6,3119,9695);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (49,"Manang",0,1,5,68);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (50,"Mustang",0,1,56,617);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (51,"Myagdi",0,4,97,490);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (52,"Baglung",0,6,428,1176);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (53,"Parbat",0,11,777,1617);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (54,"Dang",0,3,7,508);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (55,"Pyuthan",0,7,3,66);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (56,"Salyan",0,2,18,147);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (57,"Dolpa",0,0,1,5);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (58,"Mugu",0,0,0,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (59,"Humla",0,0,0,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (60,"Jumla",0,2,0,2);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (61,"Kalikot",0,0,2,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (62,"Jajarkot",0,1,0,28);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (63,"Dailekh",0,0,0,9);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (64,"Surkhet",0,3,1,31);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (65,"Bardiya",0,2,0,40);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (66,"Banke",0,0,0,81);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (67,"Kailali",0,1,0,4);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (68,"Doti",0,0,0,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (69,"Achhaam",0,0,0,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (70,"Bajura",0,0,0,1);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (71,"Bajhang",0,0,0,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (72,"Darchula",0,0,0,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (73,"Baitadi",0,0,0,0);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (74,"Dadeldhura",0,0,0,1);
+INSERT INTO district_records(id,district_name,death,injured,house_destroyed,partial_destroyed) VALUES (75,"Kanchanpur",0,1,0,0);
